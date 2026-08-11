6dc266e2667fcf3a87ca187f0843e4c008faeea7
Raghulskr12 <kalairaghul70@gmail.com>
2026-08-11 15:38:39 +0530

    feat : global exception handler added

--- src/main/java/com/cwm/studentmanagement/config/SpringConfig.java
+++ src/main/java/com/cwm/studentmanagement/config/SpringConfig.java
@@ -3,13 +3,42 @@ package com.cwm.studentmanagement.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringConfig {
+
+    private static final String[] PUBLIC_PATH = {
+            "/login",
+            "css/**",
+            "images/**",
+            "js/**",
+            "/error"
+    };
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(PUBLIC_PATH).permitAll()
+                .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                        form.loginPage("/login")
+                                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/dashboard", true)
+                )
+                .logout(logout ->logout
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());
+        return http.build();
+
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
