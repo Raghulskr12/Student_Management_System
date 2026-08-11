abe9086e43c2da10eb2e6dbc8f56815d5165626f
Raghulskr12 <kalairaghul70@gmail.com>
2026-08-11 16:20:27 +0530

    feat : global exception handler added

--- src/main/java/com/cwm/studentmanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cwm/studentmanagement/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cwm.studentmanagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.web.csrf.CsrfException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,8 +12,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(CsrfException.class)
-    public String csrfExceptionHandler(RedirectAttributes redirectAttributes){
+    public String csrfExceptionHandler(RedirectAttributes redirectAttributes,CsrfException exception) {
+        LOGGER.warn("CSRF Validation failed",exception);
         redirectAttributes.addFlashAttribute("message","Session Expired Login again");
 
         return "redirect:/login";
